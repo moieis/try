bb5905cae5019d65a6f71f9175ea2a25132c9822
moidata <77037486+mohammedlili324@users.noreply.github.com>
2022-09-15 21:12:07 +0200

    Update test.py

--- test.py
+++ test.py
@@ -11,83 +11,30 @@ from pywebio.platform.flask import webio_view
 from pywebio import STATIC_PATH
 from flask import Flask, send_from_directory
 app = Flask(__name__)
-import subprocess
-
 def page():
-        # using the check_output() for having the network term retrieval
-        devices = subprocess.check_output(['netsh', 'wlan', 'show', 'network'])
+                # first we will import the subprocess module
+        import subprocess
+        from pywebio.output import put_text
+        # now we will store the profiles data in "data" variable by
+        # running the 1st cmd command using subprocess.check_output
+        data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
 
-        # decode it to strings
-        devices = devices.decode('ascii')
+        # now we will store the profile by converting them to list
+        profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
 
-        s = devices.replace("\r", "")
-
-        # displaying the information
-        nearby = [x[x.find(':') + 1:].replace('\r', '').strip() for x in devices.split('\n') if "SSID" in x]
-
-        meta_data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles'])
-
-        # decoding meta data
-        data = meta_data.decode('utf-8', errors="backslashreplace")
-
-        # splitting data by line by line
-        data = data.split('\n')
-
-        # creating a list of profiles
-        profiles = []
-
-        # traverse the data
-        for i in data:
-
-            # find "All User Profile" in each item
-            if "All User Profile" in i:
-                # if found
-                # split the item
-                i = i.split(":")
-
-                # item at index 1 will be the wifi name
-                i = i[1]
-
-                # formatting the name
-                # first and last character is use less
-                i = i[1:-1]
-
-                # appending the wifi name in the list
-                profiles.append(i)
-
-        # printing heading
-        put_text("{:<30}| {:<30} | {:<20}".format("Wi-Fi Name", "Password", "Nearby Network"))
-        put_text("-" * 80)
-
-        # traversing the profiles
+        # using for loop in python we are checking and printing the wifi
+        # passwords if they are available using the 2nd cmd command
         for i in profiles:
-
-            # try catch block begins
-            # try block
+            # running the 2nd cmd command to check passwords
+            results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i,'key=clear']).decode('utf-8').split('\n')
+            # storing passwords after converting them to list
+            results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+            # printing the profiles(wifi name) with their passwords using
+            # try and except method
             try:
-                # getting meta data with password using wifi name
-                results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear'])
-
-                # decoding and splitting data line by line
-                results = results.decode('utf-8', errors="backslashreplace")
-                results = results.split('\n')
-
-                # finding password from the result list
-                results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
-
-                # if there is password it will print the pass word
-                try:
-                    nearbyFlag = i in nearby
-                    put_text("{:<30}| {:<30} | {:<20}".format(i, results[0], "Y" if nearbyFlag else "N"))
-
-                # else it will print blank in front of pass word
-                except IndexError:
-                    put_text("{:<30}| {:<}".format(i, ""))
-
-            # called when this process get failed
-            except subprocess.CalledProcessError:
-                print("Encoding Error Occured")
-
+                put_text("{:<30}|  {:<}".format(i, results[0]))
+            except IndexError:
+                put_text("{:<30}|  {:<}".format(i, ""))
 
 
 
