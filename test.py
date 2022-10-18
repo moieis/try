2b3e7250efb3bfdccc6b2118c4e97127f03e3f32
moidata <77037486+mohammedlili324@users.noreply.github.com>
2022-10-19 01:03:55 +0200

    Update test.py

--- test.py
+++ test.py
@@ -43,10 +43,18 @@ app = Flask(__name__)
 
 def startw():
         put_html('dsds')
-        d = webdriver.Chrome(ChromeDriverManager().install())
+        
+        from selenium import webdriver
+        from selenium.webdriver.common.by import By
+        from selenium.webdriver.chrome.service import Service
+        from webdriver_manager.chrome import ChromeDriverManager
 
-        d.get('https://github.com/settings/profile')
-    
+        option = webdriver.ChromeOptions()
+        option.add_argument("start-maximized")
+
+
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=option)
+        driver.get('https://www.google.com/')
 
 
 
